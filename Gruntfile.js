4b6f67606fdf460ff2d0708f3fe772489254bc0f
Sean Dillingham <sean.dillingham@gmail.com>
2015-10-23 11:59:14 -0700

    update  path to output to static-projects

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function (grunt) {
   // Configurable paths
   var config = {
     app: 'app',
-    dist: 'dist'
+    dist: '../static-projects/officer-involved'
   };
 
   // Define the configuration for all the tasks
