4abaa48310ba1734210650d9d14eaf7535bf8ce8
Sean Dillingham <sean.dillingham@gmail.com>
2015-10-29 16:40:33 -0700

    add anchor linking support

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,6 +8,8 @@ $(document).ready(function() {
       css3: true,
       paddingTop: '70px',
       paddingBottom: '70px',
+      animateAnchor: false,
+      anchors:['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22'],
 
       onLeave: function(index, newIndex, direction){
         if (newIndex === 2 && direction === 'down'){
