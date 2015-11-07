ad8ad1cec8cd03ff235bdced50ef8e60d6121019
Sean Dillingham <sean.dillingham@gmail.com>
2015-11-06 22:57:10 -0800

    implement expanded series nav

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,12 +5,14 @@ $(document).ready(function() {
     $('[data-toggle="modal"]').click(function(){
       var modalTarget = $(this).attr('href');
       $(modalTarget).toggleClass('active');
+      toggleViewportScroll();
       return false;
     });
       // Close all modals on ESC
       $(document).keyup(function(e) { 
           if (e.keyCode === 27) { 
               $('.modal').removeClass('active');
+              toggleViewportScroll();
           } 
       });
 
@@ -18,7 +20,21 @@ $(document).ready(function() {
     $('[data-toggle="drawer-top"]').click(function(){
       var drawerTarget = $(this).data('target');
       $(drawerTarget).toggleClass('active');
+      toggleViewportScroll();
       return false;
     });
 
+    // Prevent window scroll when modals or overlays are engaged
+    function toggleViewportScroll() {
+      if ($('body').hasClass('locked')) {
+        $('html').css('overflow', 'scroll');
+        $('body').removeClass('locked').unbind('touchmove');
+      }
+      else {
+        $('html').css('overflow', 'hidden');
+        $('body').addClass('locked').bind('touchmove', function(e) {
+            e.preventDefault()
+        });
+      }
+    } 
 });
