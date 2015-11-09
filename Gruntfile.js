b1fc60666713318dce937f5e4b1aafaa01fb9aef
Sean Dillingham <sean.dillingham@gmail.com>
2015-11-08 23:51:52 -0800

    update Gruntfile to build subdirectories

--- Gruntfile.js
+++ Gruntfile.js
@@ -247,7 +247,7 @@ module.exports = function (grunt) {
           '<%= config.dist %>/styles'
         ]
       },
-      html: ['<%= config.dist %>/{,*/}*.html'],
+      html: ['<%= config.dist %>/**/*.html'],
       css: ['<%= config.dist %>/styles/{,*/}*.css']
     },
 
@@ -291,7 +291,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= config.dist %>',
-          src: '{,*/}*.html',
+          src: '**/*.html',
           dest: '<%= config.dist %>'
         }]
       }
@@ -334,7 +334,7 @@ module.exports = function (grunt) {
           src: [
             '*.{ico,png,txt}',
             'images/{,*/}*.webp',
-            '{,*/}*.html',
+            '**/*.html',
             'styles/fonts/{,*/}*.*'
           ]
         }]
